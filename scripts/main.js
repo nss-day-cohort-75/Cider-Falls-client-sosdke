7738c76a20c80eca250e42a7cb5a870bf5f10e4c
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-01-31 20:19:48 -0600

    added getsections function

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,17 @@
 import{ generateGuestsHTML } from './guests.js'
 import { generateAttractionsHTML } from './attractions.js'
+import { generateSectionsHTML } from './sections.js'
 
 const guest = generateGuestsHTML()
 const attraction = generateAttractionsHTML()
+const section = generateSectionsHTML()
 
 const Container = document.getElementById("container")
 const render = () => {
     const html = `
     ${guest}
     ${attraction}
+    ${section}
     `
     
     Container.innerHTML = html
---
+++ scripts/sections.js
@@ -0,0 +1,15 @@
+import { getSections } from "./database.js"
+
+const sections = getSections()
+
+export const generateSectionsHTML = () => {
+        let sectionsHTML = '<section id="sections">'
+
+        sections.map(section => {
+            sectionsHTML += `<p id="${section.id}" name="${section.name}" attractionsID="${section.attractionsID}" >${section.name}</p>`
+        })
+
+        sectionsHTML += '</section>'
+
+        return sectionsHTML
+}
