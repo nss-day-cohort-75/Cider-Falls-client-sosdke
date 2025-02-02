c9baa6fa14b41768e32027d9e9f09026c99f5f49
Tanay Parekh <tanay.parekh@gmail.com>
2025-02-02 17:20:42 -0600

    unfinished services code

--- scripts/services.js
+++ scripts/services.js
@@ -1,15 +1,41 @@
-import { getServices } from './database.js'
+import { getServices, getAttractions } from "./database.js";
 
-const services = getServices()
+const services = getServices();
 
 export const generateServicesHTML = () => {
-    let servicesHTML = '<section class="service-list">Park services'
+  let servicesHTML = '<section id="services"> ';
 
-    services.map(service => {
-        servicesHTML += `<p class="services">${service.name}</p>`
-    })
+  services.map((service) => {
+    servicesHTML += `<p data-type="services" data-id="${service.id}" class="services">${service.name}</p>`;
+  });
 
-    servicesHTML += '</section>'
+  servicesHTML += "</section>";
 
-    return servicesHTML
-}
+  return servicesHTML;
+};
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  if (itemClicked.dataset.type === "services") {
+    const servicesID = parseInt(itemClicked.dataset.id);
+    const attractions = getAttractions();
+    for (const attraction of attractions) {
+      if (Array.isArray(attraction.servicesID)) {
+        for (const id of attraction.servicesID) {
+          if (id === servicesID) {
+            window.alert(
+              `${
+                itemClicked.dataset.name
+              } is available in  ${attractions.name.join(" , ")}`
+            );
+          }
+        }
+      } else if (attraction.servicesID === servicesID) {
+        window.alert(
+          `${itemClicked.dataset.name} is available in  ${attractions.name}`
+        );
+      }
+    }
+  }
+});
