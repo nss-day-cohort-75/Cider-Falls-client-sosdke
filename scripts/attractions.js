d33832098ed8c8acacdc82ad6b2db4ac3f5d2e59
Gregor Johnson <126637889+Gregor-j8@users.noreply.github.com>
2025-02-02 15:37:47 -0600

    Revert "code needs to be completed for attraction alert and main as well as CSS"

--- scripts/attractions.js
+++ scripts/attractions.js
@@ -1,33 +1,15 @@
-import { getAttractions, getGuests } from "./database.js";
+import { getAttractions } from "./database.js"
 
-const attractions = getAttractions();
+const attractions = getAttractions()
 
 export const generateAttractionsHTML = () => {
-  let attractionsHTML = '<section id="attraction"> ';
+        let attractionsHTML = '<section id="attractions">'
 
-  attractions.map((attraction) => {
-    attractionsHTML += `<p data-type="attraction" data-id="${attraction.id}" data-name="${attraction.name}" >${attraction.name} </p>`;
-  });
+        attractions.map(attraction => {
+            attractionsHTML += `<p id="${attraction.id}" name="${attraction.name}" >${attraction.name}</p>`
+        })
 
-  attractionsHTML += "</section>";
+        attractionsHTML += '</section>'
 
-  return attractionsHTML;
-};
-
-document.addEventListener("click", (clickEvent) => {
-  const itemClicked = clickEvent.target;
-
-  if (itemClicked.dataset.type === "attraction") {
-    const attractionsID = parseInt(itemClicked.dataset.id);
-    let guestCount = 0;
-    const guests = getGuests();
-    for (const guest of guests) {
-      if (guest.attractionID === attractionsID) {
-        guestCount++;
-      }
-    }
-    window.alert(
-      `There are ${guestCount} guests at ${itemClicked.dataset.name}`
-    );
-  }
-});
+        return attractionsHTML
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,23 @@
-import { generateGuestsHTML } from "./guests.js";
-import { generateAttractionsHTML } from "./attractions.js";
-import { generateSectionsHTML } from "./sections.js";
-import { generateServicesHTML } from "./services.js";
+import{ generateGuestsHTML } from './guests.js'
+import { generateAttractionsHTML } from './attractions.js'
+import { generateSectionsHTML } from './sections.js'
+import { generateServicesHTML } from './services.js'
 
-const Guests = generateGuestsHTML();
-const Attractions = generateAttractionsHTML();
-const Sections = generateSectionsHTML();
-const Services = generateServicesHTML();
+const guest = generateGuestsHTML()
+const attraction = generateAttractionsHTML()
+const section = generateSectionsHTML()
+const service = generateServicesHTML()
 
-const Container = document.getElementById("container");
+const Container = document.getElementById("container")
 const render = () => {
-  const html = `
-    <h1>Widdle Warrick Cider Falls</h1>
-    <img src = "https://preview.redd.it/mnmakkhl5xy81.jpg?width=640&crop=smart&auto=webp&s=129c1c78778eaaaca43866a76a8e4de7727785c3" alt = "ewok in park" class = "image">
-    <article class = "details">
-        <section class = "Guests">
-        <h2>Guest List</h2>
-    ${Guests}
-    </section>
-        <section class = "attractions">
-        <h2>Attraction List</h2>
-    ${Attractions}
-    </section>
-    </section>
-        <section class = "Sections">
-        <h2>Sections</h2>
-    ${Sections}
-    </section>
-    </section>
-        <section class = "Services">
-        <h2>Services</h2>
-    ${Services}
-    </section>
-    </article>
-    <footer>Phone: (615) 123-4567 || Email: widdlewarrickciderfalls@parks.gov || Address: 1 Bad Ewok Way, Nashville TN</footer>
-    `;
+    const html = `
+    ${guest}
+    ${attraction}
+    ${section}
+    ${service}
+    `
+    
+    Container.innerHTML = html
+}
 
-  Container.innerHTML = html;
-};
-
-render();
+render()
